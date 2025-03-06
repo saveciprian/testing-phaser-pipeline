4b86329317d94d7a2147f4a674be4f1c7e9567c6
Tudor Savescu <60888561+saveciprian@users.noreply.github.com>
2025-03-07 00:13:52 +0100

    figuring out rive dom GO bug

--- rive-implementation/src/importRive.js
+++ rive-implementation/src/importRive.js
@@ -4,11 +4,28 @@ export function enablePlayer()
     src: '/testing_frames.riv',
     canvas: document.getElementById("riveCanvas"),
     autoplay: true,
-    // artboard: "Arboard", // Optional. If not supplied the default is selected
+    artboard: "FlipSquare", // Optional. If not supplied the default is selected
     stateMachines: "State Machine 1",
     onLoad: () => {
       r.resizeToCanvas();
     },
   });
 }
+
+export function enableArrow(cvs)
+{
+  const r = new rive.Rive({
+    src: '/testing_frames.riv',
+    canvas: cvs,
+    autoplay: true,
+    artboard: "Swipe Arrow", 
+    stateMachines: "State Machine 1",
+    
+    onLoad: () => {
+      r.resizeToCanvas();
+    },
+  });
+}
+
+
 
